c7a0573496cf73ccf43071c5b6f938d4d68e56e9
kaiser olajuwon <kaiserjaey@gmail.com>
2023-06-18 04:16:16 +0100

    created a variable in the try block, and added the print statements.

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ import whois
 
 url = input("Enter URL \n")
 
+try:
+    domain = whois.whois(url)
+    
+except:
+    print("This Domain is available")    
+
+else:
+    print("This Domain is already purchased")
